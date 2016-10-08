1c56cb63ac185f21d07fba254aa7373680add4b1
pc <pc@pc>
2016-10-08 08:47:42 -0500

    otros

--- src/Empleado/FrontEndBundle/Entity/TurRecurso.php
+++ src/Empleado/FrontEndBundle/Entity/TurRecurso.php
@@ -23,12 +23,7 @@ class TurRecurso
     /**
      * @ORM\Column(name="codigo_recurso_tipo_fk", type="integer", nullable=true)
      */    
-    private $codigoRecursoTipoFk;    
-    
-    /**
-     * @ORM\Column(name="codigo_centro_costo_fk", type="integer", nullable=true)
-     */    
-    private $codigoCentroCostoFk;    
+    private $codigoRecursoTipoFk;        
     
     /**
      * @ORM\Column(name="codigo_empleado_fk", type="integer", nullable=true)
@@ -126,12 +121,7 @@ class TurRecurso
      * @ORM\JoinColumn(name="codigo_recurso_tipo_fk", referencedColumnName="codigo_recurso_tipo_pk")
      */
     protected $recursoTipoRel;    
-    
-    /**
-     * @ORM\ManyToOne(targetEntity="TurCentroCosto", inversedBy="recursosCentroCostoRel")
-     * @ORM\JoinColumn(name="codigo_centro_costo_fk", referencedColumnName="codigo_centro_costo_pk")
-     */
-    protected $centroCostoRel;    
+       
     
     /**
      * @ORM\OneToMany(targetEntity="TurProgramacionDetalle", mappedBy="recursoRel")
@@ -140,6 +130,8 @@ class TurRecurso
 
    
    
+    
+    
     /**
      * Constructor
      */
@@ -179,29 +171,6 @@ class TurRecurso
     public function getCodigoRecursoTipoFk()
     {
         return $this->codigoRecursoTipoFk;
-    }
-
-    /**
-     * Set codigoCentroCostoFk
-     *
-     * @param integer $codigoCentroCostoFk
-     * @return TurRecurso
-     */
-    public function setCodigoCentroCostoFk($codigoCentroCostoFk)
-    {
-        $this->codigoCentroCostoFk = $codigoCentroCostoFk;
-
-        return $this;
-    }
-
-    /**
-     * Get codigoCentroCostoFk
-     *
-     * @return integer 
-     */
-    public function getCodigoCentroCostoFk()
-    {
-        return $this->codigoCentroCostoFk;
     }
 
     /**
@@ -639,29 +608,6 @@ class TurRecurso
     public function getRecursoTipoRel()
     {
         return $this->recursoTipoRel;
-    }
-
-    /**
-     * Set centroCostoRel
-     *
-     * @param \Empleado\FrontEndBundle\Entity\TurCentroCosto $centroCostoRel
-     * @return TurRecurso
-     */
-    public function setCentroCostoRel(\Empleado\FrontEndBundle\Entity\TurCentroCosto $centroCostoRel = null)
-    {
-        $this->centroCostoRel = $centroCostoRel;
-
-        return $this;
-    }
-
-    /**
-     * Get centroCostoRel
-     *
-     * @return \Empleado\FrontEndBundle\Entity\TurCentroCosto 
-     */
-    public function getCentroCostoRel()
-    {
-        return $this->centroCostoRel;
     }
 
     /**
